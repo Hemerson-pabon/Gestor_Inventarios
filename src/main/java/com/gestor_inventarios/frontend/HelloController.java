a8b3218be5edc223481f176ff8a6228a47bbc3e8
Deivid <deividsantiago7@gmail.com>
2024-10-16 18:16:28 -0500

    Intento de Frontend :/

--- src/main/java/com/gestor_inventarios/frontend/HelloController.java
+++ src/main/java/com/gestor_inventarios/frontend/HelloController.java
@@ -1,14 +1,46 @@
 package com.gestor_inventarios.frontend;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Button btnActualizar;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Button btnEdicion;
+
+    @FXML
+    private Button btnEntrada;
+
+    @FXML
+    private Button btnInventario;
+
+    @FXML
+    private Button btnUsuarios;
+
+    @FXML
+    private Button btnVentas;
+
+    @FXML
+    private Pane pnlEdicion;
+
+    @FXML
+    private Pane pnlEntrada;
+
+    @FXML
+    private void handleClicker(ActionEvent actionEvent) {
+
+        if (actionEvent.getSource() == btnEdicion) {
+            pnlEdicion.toFront();
+        }
+        if (actionEvent.getSource() == btnEntrada){
+            pnlEntrada.toFront();
+        }
+
+}
 }
