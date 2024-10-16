a0ff65f08e8a32712d1c2fa693488ab1710b0521
Hemerson <hdpabonm@gmail.com>
2024-10-16 11:19:56 -0500

    Inicializando el proyecto

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.gestor_inventarios.gestor_inventarios {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.gestor_inventarios.gestor_inventarios to javafx.fxml;
+    exports com.gestor_inventarios.gestor_inventarios;
+}
