63151153d7e2ff171d532116b612d3fc381f45bf
Alejandro <vegaalejandro412@gmail.com>
2024-10-17 16:31:13 -0500

    actualizacion de esa mierda

--- src/main/java/com/gestor_inventarios/frontend/CambioViewController.java
+++ src/main/java/com/gestor_inventarios/frontend/CambioViewController.java
@@ -1,4 +1,41 @@
 package com.gestor_inventarios.frontend;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.CheckBox;
+
 public class CambioViewController {
+    //label's de la pantalla
+    @FXML
+    private Label valorLabel;
+
+    @FXML
+    private Label cambioLabel;
+
+    //button's de la pantalla
+    @FXML
+    private Button OtrosMetodosButton;
+
+    @FXML
+    private Button facturarButton;
+
+    @FXML
+    private Button cancelButton;
+
+    //checkbox de la pantalla
+    @FXML
+    private CheckBox efectivoCheckout;
+
+    //TextField de la pantalla
+    @FXML
+    private TextField pagoField;
+
+    //Eventos de los botones de la pantalla
+        //cancelButton
+    private void botonCerrarClickeado(){
+
+    }
+
 }
--- src/main/java/com/gestor_inventarios/frontend/FacturacionViewController.java
+++ src/main/java/com/gestor_inventarios/frontend/FacturacionViewController.java
@@ -1,4 +1,48 @@
 package com.gestor_inventarios.frontend;
-//no pude hacer esta monda, no entendi JAJAJA
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.CheckBox;
+
 public class FacturacionViewController {
+    //label's de la pantalla
+    @FXML
+    private Label labelNombreProducto;
+
+    @FXML
+    private Label precioUnitLabel;
+
+    @FXML
+    private Label totalLabel;
+
+    @FXML
+    private Label vendedorLabel;
+
+    @FXML
+    private Label totalVentaLabel;
+
+    //textField's de la pantalla
+    @FXML
+    private TextField codProductoField;
+
+    @FXML
+    private TextField cantProductoField;
+
+    //button's de la pantalla
+    @FXML
+    private Button atrasButton;
+
+    @FXML
+    private Button facturarButton;
+
+    //checkBox's de la pantalla
+    @FXML
+    private CheckBox facturaCheckout;
+
+    @FXML
+    private CheckBox ventaCheckout;;
+
+
 }
